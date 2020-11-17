2c2edf59a6bf71338e4681df7d0415b0e22299fa
AdrianDali <69985476+AdrianDali@users.noreply.github.com>
2020-11-17 13:09:31 -0600

    Add files via upload

---
+++ 17noviembre_1310/prueba_listas.py
@@ -0,0 +1,10 @@
+from linked_list import LinkedList
+
+l = LinkedList()
+print(f"esta vacia? {l.is_empty()}")
+l.append(10)
+l.append(5)
+l.append(6)
+l.append(20)
+print(f"esta vacia? {l.is_empty()}")
+l.transversal()
